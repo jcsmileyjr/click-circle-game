0a83bad43fd4d3c909c6c77a571a94da6ffd68b9
Smiley <jcsmileyjr@gmail.com>
2020-11-21 08:57:02 -0600

    functionality of displaying numbers in randomize array every 2 seconds

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
 import './App.css';
-
+import React, {useState, useEffect} from 'react';
 import HiddenCircle from './components/HiddenCircle';
 
 function App() {
+
+  //when app start, randomize an array of numbers
+  //Using an interval(), it starts the animation of a HiddenCircle
+  //If user clicks the HiddenCircle in time, update score
+
+  //const [start, setStart] = useState(false);
+  const [randomizeArray, setRandomizeArray] = useState([]);
+  const [currentArrayIndex, setArrayIndex] = useState(0);
+  
+  useEffect(() => { 
+
+    const setupGame = () => {
+      let randomizeArrayOfNumbers = [];
+      for(let i=0;i<6;i++){
+        randomizeArrayOfNumbers.push(i);
+      }
+      shuffleArray(randomizeArrayOfNumbers);
+      setRandomizeArray(randomizeArrayOfNumbers);
+    }
+
+  setupGame()}, []);
+
+  const startGame = () => {
+    let count = 0;
+    let interval = setInterval(function(){
+      console.log(randomizeArray[count]);
+      if(count < randomizeArray.length){
+        count++;
+      }else{
+        clearInterval(interval);
+      }      
+    }
+    ,2000);
+  }
+
+  //Durstenfeld shuffle technique from stack overflow
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
   return (
     <div>
       <header>
@@ -11,9 +54,9 @@ function App() {
       </header>
       <main>
         <p className="information-section-style"><span className="information-title-style">Score:</span> 100 </p>
-        <p className="information-section-style"><span className="information-title-style">Timer:</span> 1.1 sec </p>
+  <p className="information-section-style"><span className="information-title-style">Timer:</span> {currentArrayIndex} sec </p>
         <article className="center-text">
-          <button className="button-style"><label className="button-text">Play</label></button>
+          <button className="button-style" onClick={() => startGame()}><label className="button-text">Play</label></button>
         </article>        
       </main>
       <footer>
