7f59211cd602a89b5203cbf2fd3e79654d11bd69
Smiley <jcsmileyjr@gmail.com>
2020-11-21 07:11:19 -0600

    Initial styling of the circle animation

--- src/components/HiddenCircle.js
+++ src/components/HiddenCircle.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './hiddenCircle.css'
 
+/* 
+// Square shaped component with a hidden circle that when activated will display for short time frame. If the user hit it in time
+// then global score is updated
+*/
 const HiddenCircle = (props) => {
+    const [animateCircle, showAnimateCircle] = useState(false); // Start the animation
+
+    // Function to start and stop the animation and allow user to score
+    const startAnimation =() => {
+        showAnimateCircle(true)
+    }
     return(
         <main className="circle-container" id={props.circleID}>
-            <section className="inner-circle"></section>
+            <section className={animateCircle?`inner-circle circle-animation`:`inner-circle `} onClick={startAnimation}></section>
         </main>
     );
 }
