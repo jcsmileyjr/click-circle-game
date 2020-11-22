e20adf67408d5fac27008e01f317f9d7924f37a5
Smiley <jcsmileyjr@gmail.com>
2020-11-21 23:14:15 -0600

    Feature: Add a difficulty leveling

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [animateCircle, setanimateCircle] = useState("");
   const [score, updateScore] = useState(0);
   const [correct, setCorrect] = useState([]);
+  const [difficulty, setDifficulty] = useState("easy");
   
   useEffect(() => { 
     //Create array of numbers, then shuffle them, and finally save to global state
@@ -24,6 +25,8 @@ function App() {
 
   const startGame = () => {
     updateScore(0); // Reset the score
+    setCorrect([]) // Reset showing crowns icons for correct choicesf
+    let test = determineDifficulty();
     let count = 0;
     let interval = setInterval(function(){
       let currentCircle = randomizeArray[count]; // current random number from the currnt index in array
@@ -36,7 +39,17 @@ function App() {
         clearInterval(interval);
       }      
     }
-    ,1000);
+    ,test);
+  }
+
+  const determineDifficulty = () => {
+    if(difficulty === "easy"){
+      return 900;
+    }else if(difficulty === "medium"){
+      return 700;
+    }else{
+      return 600
+    }
   }
 
   // If the user click the animated circle, with correct id, within the time frame of the startGame(), then add to score
@@ -90,7 +103,16 @@ function App() {
           <HiddenCircle activate = {animateCircle === 9?true:false} circleId={9} click={userClick} correctPicked={correct.includes(9)?true:false} />
           <HiddenCircle activate = {animateCircle === 10?true:false} circleId={10} click={userClick} correctPicked={correct.includes(10)?true:false} />
           <HiddenCircle activate = {animateCircle === 11?true:false} circleId={11} click={userClick} correctPicked={correct.includes(11)?true:false} />
-        </section>                
+        </section> 
+        <section className="row-of-hidden-circles">
+          <label className="information-title-style">Difficulty Level: </label>
+          <input type="radio" id="easy" name="difficulty" value="Easy" checked={difficulty === "easy"} onChange={() => setDifficulty("easy")} />
+          <label htmlFor="easy">Easy</label>
+          <input type="radio" id="medium" name="difficulty" value="medium" checked={difficulty === "medium"} onChange={() => setDifficulty("medium")} />
+          <label htmlFor="medium">Medium</label>
+          <input type="radio" id="hard" name="difficulty" value="hard" checked={difficulty === "hard"} onChange={() => setDifficulty("hard")} />
+          <label htmlFor="hard">Hard</label>
+        </section>               
       </footer>
     </div>
   );
