8f6683c1a2be6f504e083b96b0d784a8b76c9dc5
Smiley <jcsmileyjr@gmail.com>
2020-11-22 14:48:30 -0600

    Remove un-needed code

--- src/App.js
+++ src/App.js
@@ -72,7 +72,6 @@ function App() {
         </header>
         <main>
           <p className="information-section-style"><span className="information-title-style">Score:</span> {score}/120 </p>
-          {/*<p className="information-section-style"><span className="information-title-style">Timer:</span> 1.1 sec </p>*/}
           <article className="center-text">
             <button className="button-style" disabled={disablePlayButton} onClick={() => startGame()}><label className="button-text">Play</label></button>
           </article>        
