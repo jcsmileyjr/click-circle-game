f09268dbae4b3eefa24456c11a669e2aa4bd4bba
Smiley <jcsmileyjr@gmail.com>
2020-11-21 20:37:18 -0600

    Removed timer

--- src/App.js
+++ src/App.js
@@ -57,11 +57,11 @@ function App() {
     <div>
       <header>
         <h1 className="center-text">Click a Circle</h1>
-        <p className="instructions-style"><span className="instructions-title-style">Instructions</span>: Click the circle befofre it disappears. Each win adds 2 seconds to the timer</p>
+        <p className="instructions-style"><span className="instructions-title-style">Instructions</span>: Click the circle before it disappears. Each win adds 2 seconds to the timer</p>
       </header>
       <main>
-        <p className="information-section-style"><span className="information-title-style">Score:</span> {score} </p>
-        <p className="information-section-style"><span className="information-title-style">Timer:</span> 1.1 sec </p>
+        <p className="information-section-style"><span className="information-title-style">Score:</span> {score}/120 </p>
+        {/*<p className="information-section-style"><span className="information-title-style">Timer:</span> 1.1 sec </p>*/}
         <article className="center-text">
           <button className="button-style" onClick={() => startGame()}><label className="button-text">Play</label></button>
         </article>        
